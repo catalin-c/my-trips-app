f442ad316b3f02e699b0bcd125d3bbff2d6ade12
catalin-c <c_chirila97@yahoo.com>
2018-04-26 18:23:41 +0300

    trip page first ajax call

--- src/main/resources/static/plugins/ajaxreq.js
+++ src/main/resources/static/plugins/ajaxreq.js
@@ -1,6 +1,14 @@
-alert($("#tripSelect :selected").text());
+$("#tripSelect :selected").text();
 
 
 $('#tripSelect').on('change', function() {
-    alert( this.value );
+    // alert( this.value );
+    sParameter = this.value;
+    sParameter = encodeURIComponent(sParameter.trim());
+    // console.log(sParameter);
+    $.ajax({url: "http://localhost:7070/trip?tripName=" + sParameter, success: function(result){
+        $("#impressionsText").text(result['impression']);
+        $("#datesText").text("From: " + result['dateFrom'] + " to " + result['dateTo']);
+
+    }});
 });
