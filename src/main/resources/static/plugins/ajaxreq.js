290f2b52f721b3ee97e012e8558edb4f9cfb3cf7
catalin-c <c_chirila97@yahoo.com>
2018-04-27 00:39:48 +0300

    add photos dynamically on my trips page

--- src/main/resources/static/plugins/ajaxreq.js
+++ src/main/resources/static/plugins/ajaxreq.js
@@ -1,14 +1,17 @@
 $( document ).ready(function() {
 
-    function populateOnPageLoad() {
-        sParameter = encodeURIComponent($("#tripSelect :selected").text().trim());
+
+    var sParameter = encodeURIComponent($("#tripSelect :selected").text().trim());
+    // var tripId = 0;
+
+    function populatePageFirst(sParameter) {
 
         $.ajax({
             type: "get", url: "http://localhost:7070/trip?tripName=" + sParameter,
             success: function(result){
                 $("#impressionsText").text(result['impression']);
                 $("#datesText").text("From: " + result['dateFrom'] + " to " + result['dateTo']);
-
+                populatePageSecond(result['id']);
             },
 
             error: function (request) {
@@ -17,28 +20,34 @@ $( document ).ready(function() {
             }
 
         });
+
     }
 
-    populateOnPageLoad();
+    function populatePageSecond(tripId) {
+        $.ajax({
+            type: "get", url: "http://localhost:7070/photos?tripId=" + tripId,
+            success: function(resultTwo){
+                $( '<div class="col-md-4 photoImage"> <img src="' + resultTwo[0]['photoLink'] + '" alt="photo" class="img-thumbnail"> <h3>' + resultTwo[0]['title'] + '</h3></div>' ).insertAfter( "#googleMap" );
+
+                console.log(resultTwo[0]['photoLink']);
+                console.log(resultTwo[0]['title']);
+            },
+
+            error: function (resultTwo) {
+                // $("#impressionsText").text("Error loading impressions");
+                // $("#datesText").text("Error loading dates");
+
+            }
+        });
+    }
+
+    populatePageFirst(sParameter);
 
     //On trip selector change
     $('#tripSelect').on('change', function() {
 
-        sParameter = encodeURIComponent(this.value.trim());
-
-        $.ajax({
-            type: "get", url: "http://localhost:7070/trip?tripName=" + sParameter,
-            success: function(result){
-                $("#impressionsText").text(result['impression']);
-                $("#datesText").text("From: " + result['dateFrom'] + " to " + result['dateTo']);
-
-            },
-
-            error: function (request) {
-                $("#impressionsText").text("Error loading impressions");
-                $("#datesText").text("Error loading dates");
-            }
-
-        });
+        var parameter = encodeURIComponent(this.value.trim());
+        $(".photoImage").remove()
+        populatePageFirst(parameter);
     });
 });
