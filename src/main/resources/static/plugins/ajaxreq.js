d4f6f0f6b7e235ecc7150bad8aefe7f8b259a0d5
catalin-c <c_chirila97@yahoo.com>
2018-04-29 18:53:34 +0300

    add photo form

--- src/main/resources/static/plugins/ajaxreq.js
+++ src/main/resources/static/plugins/ajaxreq.js
@@ -54,14 +54,9 @@ $( document ).ready(function() {
 
         $(".photoImage").remove()
         populatePageFirstPart(parameter);
-        // map.setCenter(new google.maps.LatLng(44.439663, 26.096306));
     });
 
-
-
-
-
-
+    //
     $( "#deleteTrip" ).click(function() {
         var answer = confirm("Are you sure you want to delete " + $("#tripSelect :selected").text() + "?");
         if (answer) {
@@ -78,33 +73,31 @@ $( document ).ready(function() {
             });
         }
     });
-
-    //JS google map
-    // var map;
-    // function initMap() {
-    //     var latitude = 27.7172453; // YOUR LATITUDE VALUE
-    //     var longitude = 85.3239605; // YOUR LONGITUDE VALUE
     //
-    //     var myLatLng = {lat: latitude, lng: longitude};
-    //
-    //     map = new google.maps.Map(document.getElementById('map'), {
-    //         center: myLatLng,
-    //         zoom: 14
-    //     });
-    //
-    //     var marker = new google.maps.Marker({
-    //         position: myLatLng,
-    //         map: map,
-    //         //title: 'Hello World'
-    //
-    //         // setting latitude & longitude as title of the marker
-    //         // title is shown when you hover over the marker
-    //         title: latitude + ', ' + longitude
-    //     });
-    // }
-    //
-    // google.maps.event.addDomListener(window,'load', initMap());
+    // $("#addPhoto").click(function () {
+    //     // $("#test").load("http://localhost:7070/profile").dialog({modal:true});
+    // });
 
 
+
+
+
+    //Add Photo Form
+    $("#addPhoto").click(function() {
+        $("#addPhotoDiv").css("display", "block");
+    });
+    $("#addPhotoForm #cancel").click(function() {
+        $(this).parent().parent().hide();
+    });
+
+    $("#send").click(function() {
+        var photoName = $("#photoName").val();
+        var photoLink = $("#email").val();
+        if (photoName == "" || photoLink == ""){
+            alert("Please fill all the fields!");
+        }else{
+            $("#addPhotoDiv").css("display", "none");
+        }
+    });
 
 });
